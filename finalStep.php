36da5bec1d2def6364d6c9c087e5ed5e62258a9e
B00152481 Umar Raheem <B00152481@mytudublin.ie>
2025-11-19 10:50:44 +0000

    Completed Part 2

--- finalStep.php
+++ finalStep.php
@@ -4,25 +4,25 @@ include("finalStepHeader.html");
 //translate the comments below into PHP code underneath each comment
 
 //start a session
-
+session_start();
 //echo the passenger's firstname from the appropriate session variable
-
+echo $_SESSION['passengerFN'];
 echo "<BR>";
 
 //echo the passenger's surname from the appropriate session variable
-
+echo $_SESSION['passengerSN'];
 echo "<BR>";
 
 // if the luggage session variable is on
-
+if (isset($_SESSION['luggage'])){
     //echo the amount of bags under ten kilos the passenger is bringing
-    
+    echo $_SESSION['subTenKG'];
     echo "<BR>";
-    
+   
     //echo the amount of bags over ten kilos the passenger is bringing
-    
+    echo $_SESSION['overTenKG'];
 //end if block
-
+}
 ?>
 </h4></div></div>
 <form method="POST" action="confirm.php">
@@ -33,10 +33,10 @@ echo "<BR>";
         </div>
     </div>
    
-        
+       
       <div class="col-sm-12 text-center">
         <button type="submit" class="btn btn-default">Continue</button>
       </div>
     </div>
-    
+   
 </form>
